c21f1b313fdedf92d78c257be767774691f7e308
Afonso Araujo <afonso.henriquedearaujo@gmail.com>
2026-06-16 15:59:40 -0300

    Estrutura inicial do Front e criação da pasta Backend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { Login } from './Frontend/login/login';
+import { Schools } from './Frontend/schools/schools';
+import { Students } from './Frontend/students/students';
+import { Teachers } from './Frontend/teachers/teachers';
+import { Users } from './Frontend/users/users';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: Login
+  },
+  {
+    path: "login",
+    component: Login
+  },
+  {
+    path: "schools",
+    component: Schools
+  },
+  {
+    path: "students",
+    component: Students
+  },
+  {
+    path: "teachers",
+    component: Teachers
+  },
+  {
+    path: "users",
+    component: Users
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './Frontend/login/login';
+import { Schools } from "./Frontend/schools/schools";
+import { Users } from './Frontend/users/users';
+import { Students } from './Frontend/students/students';
+import { Teachers } from './Frontend/teachers/teachers';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Login, Schools, Students, Teachers, Users],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
